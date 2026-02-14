726ad3100a90b1f76290e60d445bcbb6f4bb44a9
Vasileios Moratis <vassi07@gmail.com>
2026-02-14 17:31:01 +0100

    Testing things locally without the API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [guestList, setGuestlist] = useState([]);
 
-const [firstName,setFirstName]= useState("");
-const [lastName,setLastName]=useState(" ")
+
+  function handleToggle({guest}){
+    setGuestlist([...guestList,guest.isAttending===true])
+
+  }
+
+  function handleDelete(guest) {
+    setGuestlist(guestList.filter((member) => member.id !== guest.id));}
+
+  //    useEffect(() => {
+  //   setGuestlist(" ");
+  // }, []);
+
+  function handleSubmit() {
+    const id = guestList.length + 1;
+      setGuestlist([
+      ...guestList,
+      {
+        id: id,
+        firstName: firstName,   // The data structure is important
+        lastName: lastName,
+        isAttending: false,
+      },
+    ]);
+    setFirstName("");
+    setLastName("");
 
 
-//   Create guest on form submission line 17
+  }
+
+
 
   return (
     <div className="App">
+      <header>This is a Guest List App</header>
 
-
-      <form >
-        <label htmlFor='first'>First Name</label>
-        <input type="text" value={firstName}></input>
-        <label htmlFor='last'>Last Name</label>
-        <input type="text" value={lastName}id="last"></input>
+      <form
+        style={{ marginTop: '50px' }}
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <label htmlFor="first">First Name</label>
+        <input
+          type="text"
+          id="first"
+          value={firstName}
+          onChange={(event) => setFirstName(event.currentTarget.value)}
+        />
+        <label htmlFor="last">Last Name</label>
+        <input
+          type="text"
+          value={lastName}
+          id="last"
+          onChange={(event) => setLastName(event.currentTarget.value)}
+        />
+        <button type="submit">Submit</button>
       </form>
 
+      {guestList.map((guest) => {
+        return (
+          <div key={guest.id} data-test-id="guest">
+            {guest.firstName} {guest.lastName}{' '}
+            {guest.isAttending ? 'Attending' : 'Not Attending'}
+            <button onClick={()=>handleDelete(guest)}>Delete</button>
+            <input type="checkbox"
+                   aria-label ={`${guest.firstName} ${guest.lastName} attending status`}
+                   onChange={handleToggle(guest)}>
+
+              </input>
+          </div>
+        );
+      })}
     </div>
   );
 }
