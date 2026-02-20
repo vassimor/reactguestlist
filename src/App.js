bf7d4156235a7e0e4b9ebe822c6754740fb6249d
Vasileios Moratis <vassi07@gmail.com>
2026-02-20 19:47:07 +0100

    Implement  Update and Delete functions

--- src/App.js
+++ src/App.js
@@ -6,43 +6,65 @@ export default function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [guestList, setGuestlist] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const baseUrl = 'https://express-guest-list-api-memor-rppfmfj6w2ky.vassimor.deno.net/';
 
-     function handleToggle(id,attending){
-    //  setGuestlist([...guestList,guest.isAttending===true])
-    const newguestList=[...guestList];
-    console.log(newguestList);
-    const attendingGuest=newguestList.find((guest)=>guest.id===id)
-    attendingGuest.isAttending=attending;
-    console.log(attendingGuest);
-    console.log(attending);
-    // setGuestlist(newguestList)
-
-  }
-
-  function handleDelete(guest) {
-    setGuestlist(guestList.filter((member) => member.id !== guest.id));}
-
-
-
-  function handleSubmit() {
-    const id = guestList.length + 1;
-      setGuestlist([
-      ...guestList,
-      {
-        id: id,
-        firstName: firstName,   // The data structure is important
-        lastName: lastName,
-        isAttending: false,
+  async function handleToggle(id, isAttending) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    ]);
-    setFirstName("");
-    setLastName("");
+      body: JSON.stringify({ attending: !isAttending }),
+    });
+    const updatedGuest = await response.json();
 
+    const newGuestList = guestList.filter(
+      (guest) => guest.id !== updatedGuest.id,
+    );
 
+    setGuestlist([...newGuestList, updatedGuest]);
   }
 
+  async function handleDelete(id) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'DELETE',
+    });
+    const deletedGuest = await response.json();
 
+    setGuestlist(guestList.filter((person) => person.id !== deletedGuest.id));
+  }
+
+  async function handleSubmit() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ firstName: firstName, lastName: lastName }),
+    });
+
+    const createdGuest = await response.json();
+
+    setGuestlist([...guestList, createdGuest]);
+
+    setFirstName('');
+    setLastName('');
+  }
+
+  async function fetchGuests() {
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+    setGuestlist(allGuests);
+  }
+
+  useEffect(() => {
+    console.log('inside use effect', guestList);
+    fetchGuests();
+    console.log('after fetch', guestList);
+    setIsLoading(false)
+  }, []);
 
   return (
     <div className="App">
@@ -50,20 +72,20 @@ export default function App() {
 
       <form
         style={{ marginTop: '50px' }}
-        onSubmit={(event) => {
+        onSubmit={async (event) => {
           event.preventDefault();
-          handleSubmit();
+          await handleSubmit();
         }}
       >
         <label htmlFor="first">First Name</label>
-        <input
+        <input disabled={isLoading}
           type="text"
           id="first"
           value={firstName}
           onChange={(event) => setFirstName(event.currentTarget.value)}
         />
         <label htmlFor="last">Last Name</label>
-        <input
+        <input disabled={isLoading}
           type="text"
           value={lastName}
           id="last"
@@ -72,27 +94,24 @@ export default function App() {
         <button type="submit">Submit</button>
       </form>
 
-      {guestList.map((guest) => {
-        return (
-          <div key={guest.id} data-test-id="guest">
-            {guest.firstName} {guest.lastName}{' '}
-            {guest.isAttending ? 'Attending' : 'Not Attending'}
-            <button onClick={()=>handleDelete(guest)}>Delete</button>
-            <input type="checkbox" 
-                   aria-label ={`${guest.firstName} ${guest.lastName} attending status`}
-
-            onChange={()=>{
-              console.log("guestid",guest.id)
-
-              // handleToggle(guest.id,!guest.isAttending)
-              }}
-
-                  >
-
-              </input>
-          </div>
-        );
-      })}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        guestList.map((guest) => {
+          return (
+            <div key={guest.id} data-test-id="guest">
+              {guest.firstName} {guest.lastName}{' '}
+              {guest.attending ? 'Attending' : 'Not Attending'}
+              <button onClick={() => handleDelete(guest.id)}>Delete</button>
+              <input
+                type="checkbox"
+                aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+                onChange={() => handleToggle(guest.id, guest.attending)}
+              ></input>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
